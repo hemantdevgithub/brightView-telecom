b24e10901832c2af541aafb43ba6ed3536fa98e2
imrannaazir <imrannaaziremon@gmail.com>
2024-09-02 18:12:55 +0600

    [added]

---
+++ src/components/Home/WhatWeDo.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Container from "../Container/Container";
+
+const WhatWeDo = () => {
+  return (
+    <Container>
+      <section className="flex flex-col md:flex-row items-center py-8 ">
+        {/* Right Side: Content */}
+        <div className="w-full md:w-1/2 py-5">
+          <div className="px-4">
+            <div
+              className="text-left"
+              data-wow-delay="0.1s"
+              style={{ marginBottom: 70 }}
+            >
+              <div className="mb-3">
+                <h5 className="text-lg font-semibold text-secondary bg-gray-100 px-4 py-2 rounded-full inline-block">
+                  What We Do?
+                </h5>
+              </div>
+              <h1 className="text-4xl font-bold mb-4 text-primary">
+                Our Expertise & Services
+              </h1>
+              <p className="text-lg text-gray-500 text-left mb-3">
+                At Brightview Telecom, we excel in delivering top-notch
+                solutions across the Telecom and IT sectors. Our expertise
+                includes the installation of BTS, BSC, MSC, Access Networks, RF
+                Drive testing and optimization, Data Center setups, and FTTH
+                solutions.
+              </p>
+              <p className="text-lg text-gray-500 text-left">
+                We are also leaders in IT solutions, specializing in Cloud
+                services, App development, ERP systems, and more. In addition,
+                we provide highly skilled technical and non-technical manpower
+                to meet your project needs.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Left Side: Image */}
+        <div className="w-full md:w-[40%]">
+          <img
+            src="/Home/what-we-do.jpg"
+            alt="Description of the image"
+            className="rounded-lg shadow-lg w-full h-auto"
+          />
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default WhatWeDo;
---
+++ src/components/Home/WhoWeAre.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Container from "../Container/Container";
+
+const WhoWeAre = () => {
+  return (
+    <Container>
+      <section className="flex flex-col md:flex-row items-center py-8 ">
+        {/* Left Side: Image */}
+        <div className="w-full md:w-[40%]">
+          <img
+            src="/Home/who-we-are.jpg"
+            alt="Description of the image"
+            className="rounded-lg shadow-lg w-full h-auto"
+          />
+          a
+        </div>
+
+        {/* Right Side: Content */}
+        <div className="w-full md:w-1/2 py-5">
+          <div className="px-4">
+            <div className="text-left mb-8" data-wow-delay="0.1s">
+              <div className="mb-3">
+                <h5 className="text-lg font-semibold text-secondary bg-gray-100 px-4 py-2 rounded-full inline-block">
+                  Who We Are?
+                </h5>
+              </div>
+              <h1 className="text-4xl font-bold mb-4 text-primary">
+                Trusted Telecom and IT Solutions Provider Since 2008
+              </h1>
+              <p className="text-lg text-gray-500 text-left mb-3">
+                Brightview Telecom was incorporated in 2008. Since then,
+                Brightview has been providing various solutions to its clients
+                in the field of Telecom, IT, and Manpower.
+              </p>
+              <p className="text-lg text-gray-500 text-left">
+                With specialized delivery teams, experienced management, and a
+                long-term vision, Brightview is a respected name in the field of
+                Telecom and IT.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default WhoWeAre;
