46ec7481b7260e280b1838d33d5c7dc979a6c06a
imrannaazir <imrannaaziremon@gmail.com>
2024-09-02 18:30:29 +0600

    added: image enlargement

--- src/pages/Awards.jsx
+++ src/pages/Awards.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Container from "../components/Container/Container";
 import TopBanner from "../components/Shared/TopBanner/TopBanner";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 const Awards = () => {
   const certificates = [
@@ -25,16 +27,15 @@ const Awards = () => {
       <Container>
         <div className="py-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 mx-auto">
           {certificates.map((x, i) => (
-            <div className="relative group border rounded-xl overflow-hidden">
-              <img
-                className="h-[300px] w-full object-cover transition-transform duration-300 group-hover:scale-110"
-                src={x}
-                alt={`Certificate ${i + 1}`}
-              />
-              <div className="absolute inset-0 bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white text-center">
-                <p className="text-lg">Award</p>
-              </div>
-            </div>
+            <PhotoProvider>
+              <PhotoView src={x}>
+                <img
+                  className="h-[300px] w-full object-cover transition-transform duration-300 group-hover:scale-110 rounded-lg"
+                  src={x}
+                  alt=""
+                />
+              </PhotoView>
+            </PhotoProvider>
           ))}
         </div>
       </Container>
@@ -43,3 +44,15 @@ const Awards = () => {
 };
 
 export default Awards;
+
+/* 
+
+
+
+
+
+
+
+
+
+*/
