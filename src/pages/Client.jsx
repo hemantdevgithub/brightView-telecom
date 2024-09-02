4932ef93d2c10b4deabda2afefdbed4bfc4e4352
imrannaazir <imrannaaziremon@gmail.com>
2024-09-02 17:10:16 +0600

    modified

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -1,57 +1,49 @@
-import React from 'react';
-import Container from '../components/Container/Container';
-import TopBanner from '../components/Shared/TopBanner/TopBanner';
+import React from "react";
+import Container from "../components/Container/Container";
+import TopBanner from "../components/Shared/TopBanner/TopBanner";
+import OurClients from "../components/Home/OurClients";
+import TechnologyPartners from "../components/Home/TechnologyPartners";
+import TelecomClients from "../components/Home/TelecomClients";
 
 const Client = () => {
   return (
     <div>
-      <TopBanner pageName={"Our Clients"} img={"https://supportbrightviewtelecom.netlify.app/about.jpg"} heading={"Clients"} />
+      <TopBanner
+        pageName={"Our Clients"}
+        img={"https://supportbrightviewtelecom.netlify.app/about.jpg"}
+        heading={"Clients"}
+      />
       <Container>
-        <div className='py-10 font-poppins space-y-8'>
-          <h4 className='text-center text-xl font-semibold uppercase w-fit mx-auto pb-1 border-b-4 border-primary px-4'>Our Clients
+        <div className="py-10 font-poppins space-y-8">
+          <h4 className="text-center text-xl font-semibold uppercase w-fit mx-auto pb-1 border-b-4 border-primary px-4">
+            Our Clients
           </h4>
-          <div className='w-[60%] mx-auto space-y-5'>
-            <p className='text-gray-500 space-y-2 *:block'>
-              <span>  Approved By All Major OEMs Including Nokia, Huawei, Ericsson, ZTE, Alcatel, NEC, Samsung, NEC Ceragon Etc.</span>
+          <div className="w-[60%] mx-auto space-y-5">
+            <p className="text-gray-500 space-y-2 *:block">
+              <span>
+                {" "}
+                Approved By All Major OEMs Including Nokia, Huawei, Ericsson,
+                ZTE, Alcatel, NEC, Samsung, NEC Ceragon Etc.
+              </span>
 
-              <span>Worked With All Major Operators Including Airtel , Vodafone, TTSL, RJIL, Samsung, Idea, MTS, Uninor, Videocon, Aircel, BSNL, MTNL Etc.</span>
+              <span>
+                Worked With All Major Operators Including Airtel , Vodafone,
+                TTSL, RJIL, Samsung, Idea, MTS, Uninor, Videocon, Aircel, BSNL,
+                MTNL Etc.
+              </span>
 
               <span>Working In Nepal For Huawei & ZTE For Last 5 Years.</span>
 
               <span>Project Executed In 10 Circles Of India</span>
 
               <span>Most Valued Partner Of Huawei For Last 3 Years.</span>
-
             </p>
           </div>
         </div>
-        <div className='py-10 font-poppins space-y-8'>
-          <h4 className='text-center text-xl font-semibold uppercase w-fit mx-auto pb-1 border-b-4 border-primary px-4'>Telecom Clients
-          </h4>
-          <div className='w-[60%] mx-auto space-y-5'>
-            <h1 className='text-4xl font-semibold tracking-wide'>
-              What Our Clients Say
-            </h1>
-            <p className='text-gray-500 space-y-2 *:block'>
-              Our clients trust BrightViewTelecom for our reliable and innovative telecommunication solutions.
-            </p>
-          </div>
-          <span>here will slider of telecom client</span>
-        </div>
-        <div className='py-10 font-poppins space-y-8'>
-          <h4 className='text-center text-xl font-semibold uppercase w-fit mx-auto pb-1 border-b-4 border-primary px-4'>Technology Partner
-          </h4>
-          <div className='w-[60%] mx-auto space-y-5'>
-            <h1 className='text-4xl font-semibold tracking-wide'>
-              What Our Technology Partner say
-            </h1>
-          </div>
-          <span>here will slider of tech partner</span>
-        </div>
-
-      </Container >
-
-    </div >
+      </Container>
+      <TechnologyPartners />
+      <TelecomClients />
+    </div>
   );
 };
 
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,52 +1,56 @@
-import React from 'react';
-import Container from '../components/Container/Container';
-import TopBanner from '../components/Shared/TopBanner/TopBanner';
-
-
+import React from "react";
+import Container from "../components/Container/Container";
+import TopBanner from "../components/Shared/TopBanner/TopBanner";
+import ProvidedSolutions from "../components/Home/ProvidedSolutions";
 
 const Services = () => {
-    return (
-        <div>
-            <TopBanner pageName={"Services"} img={"https://supportbrightviewtelecom.netlify.app/about.jpg"} heading={"Services"} />
-            <Container>
-                <div className='py-10 font-poppins space-y-8'>
-                    <h4 className='text-center text-xl font-semibold uppercase w-fit mx-auto pb-1 border-b-4 border-primary px-4'>Our Services
-                    </h4>
-                    <div className='w-[60%] mx-auto space-y-5'>
-                        <h1 className='text-4xl font-semibold tracking-wide'>
-                            Enabling Your Immigration Successfully
+  return (
+    <div>
+      <TopBanner
+        pageName={"Services"}
+        img={"https://supportbrightviewtelecom.netlify.app/about.jpg"}
+        heading={"Services"}
+      />
+      <Container>
+        <div className="py-10 font-poppins space-y-8">
+          <h4 className="text-center text-xl font-semibold uppercase w-fit mx-auto pb-1 border-b-4 border-primary px-4">
+            Our Services
+          </h4>
+          <div className="w-[60%] mx-auto space-y-5">
+            <h1 className="text-4xl font-semibold tracking-wide">
+              Enabling Your Immigration Successfully
+            </h1>
+            <p className="text-gray-500 space-y-2 *:block">
+              <span>
+                Installation Of BTS, BSC, MSC, Server, Access Network, DWDM And
+                SDH Network, GPON,FTTH Equipment Installation And Commissioning
+              </span>
 
-                        </h1>
-                        <p className='text-gray-500 space-y-2 *:block'>
-                            <span>Installation Of BTS, BSC, MSC, Server, Access Network, DWDM And SDH Network, GPON,FTTH Equipment Installation And Commissioning</span>
+              <span>
+                Installation Of Multi Service Access Network (Wire Line)
+                Equipment Installation And Commissioning.
+              </span>
 
-                            <span>Installation Of Multi Service Access Network (Wire Line) Equipment Installation And Commissioning.</span>
+              <span>
+                Experience In RF Drive Test And Optimization, SCFT, SSCVT, EMF,
+                NARDA Broadband Testing And NBM Activity.
+              </span>
 
-                            <span>Experience In RF Drive Test And Optimization, SCFT, SSCVT, EMF, NARDA Broadband Testing And NBM Activity.</span>
-
-                            <span>Good Experience In Huawei Optix 1832 X-8/16/7500/8800/9800 /500 /550 ,Alcatel Lucent _Nokia – 7750 SR-14s / 7750 SR-7s / 7750 SR-12e / 7250 IXR-R6 / 7210 SAS-K30 / 7210 SAS-MXP In OptiX Multi-Service Mini-WDM/OTN/OSN/CEN System Combines The OTN And WDM Features, Which Enables Customers To Integrate Multiple Access Transport Networks Into A Single Network</span>
-                        </p>
-                    </div>
-                    <div className='grid grid-cols-4 gap-5'>
-                        {
-                            Array.from({ length: 8 }).map(x => (
-                                <div className='rounded-xl overflow-hidden border'>
-                                    <img
-                                        className='h-[200px] w-full object-cover'
-                                        src="https://supportbrightviewtelecom.netlify.app/img/service-1.jpg" alt="" />
-                                    <div className='p-4'>
-                                        <h3 className='text-xl font-semibold'>Civil Work</h3>
-                                        <p className='text-sm text-gray-700'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque ex, aut ipsa a doloremque vel, itaque enim distinctio </p>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-            </Container>
-
+              <span>
+                Good Experience In Huawei Optix 1832 X-8/16/7500/8800/9800 /500
+                /550 ,Alcatel Lucent _Nokia – 7750 SR-14s / 7750 SR-7s / 7750
+                SR-12e / 7250 IXR-R6 / 7210 SAS-K30 / 7210 SAS-MXP In OptiX
+                Multi-Service Mini-WDM/OTN/OSN/CEN System Combines The OTN And
+                WDM Features, Which Enables Customers To Integrate Multiple
+                Access Transport Networks Into A Single Network
+              </span>
+            </p>
+          </div>
+          <ProvidedSolutions />
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Services;
